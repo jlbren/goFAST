fec9556f99ea0c2aa5d7b5bf0ec4e6847e37cc3c
Jon Brenner <jbrenluc@gmail.com>
2016-11-28 03:05:56 -0600

    pmc api abstract via json get. output in console. todo: parse reponse object for fields to be passed to twitter

--- testjson.js
+++ testjson.js
@@ -1,12 +1,16 @@
 function testJSON(query){
-alert(query.value);
-console.log("test");       	
-$.getJSON("trips.json", function(trip) {
- console.log("success"); 	
- console.log(trip);
-})
-.fail(function (jqxhr, status, error) {
-        console.log("fail"); 	
-	console.log('error', status, error) }
-);
+	var pmcAPI="https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&rettype=abstract&id=";
+	var  pmcURL=pmcAPI + query.value ; 
+	console.log("PMC query: " + pmcURL); 
+	$.getJSON(pmcURL, function(abs) {
+		{format: "json"}
+ 		console.log("success"); 	
+ 		console.log(JSON.stringify(abs));
+	       alert("Dev note: check console for output"); 	
+	})
+	.fail(function (jqxhr, status, error) {
+        	console.log("fail"); 	
+		console.log('error', status, error) }
+	);
 };
+
