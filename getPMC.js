85a1e826c322dd73df753ce7f87e16fce3e1cf15
Jon Brenner <jbrenluc@gmail.com>
2016-11-28 03:17:09 -0600

    renamed js file/functions

--- getPMC.js
+++ getPMC.js
@@ -1,4 +1,4 @@
-function testJSON(query){
+function getPMC(query){
 	var pmcAPI="https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&rettype=abstract&id=";
 	var  pmcURL=pmcAPI + query.value ; 
 	console.log("PMC query: " + pmcURL); 
